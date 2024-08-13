3092dec1208dc22fa8b6bbe6156ca159cd92c5a8
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-08-13 08:44:22 +0200

    Basic template from Squid AI with TS backend. CRM customization to be done

---
+++ services-crm/backend/src/service/aiQuery.ts
@@ -0,0 +1,19 @@
+import { SquidService, secureDatabase, executable } from '@squidcloud/backend';
+
+export class ExampleService extends SquidService {
+  @secureDatabase('all', 'built_in_db')
+  allowAccessToBuiltInDb(): boolean {
+    return true;
+  }
+
+  @executable()
+  async askQuestion(question:string): Promise<string> {
+    const aiResponse = await this.squid.ai().executeAiQuery('built_in_db', question);
+
+    console.log(`Question: ${question}
+      Query: ${aiResponse.executedQuery ?? 'No query executed'}
+      Explaination: ${aiResponse.explanation ?? 'No explaination'}`)
+
+      return aiResponse.answer
+  }
+}
---
+++ services-crm/frontend/src/types.ts
@@ -0,0 +1,7 @@
+export type Trip = {
+  endDate: Date;
+  notes: string[];
+  country: string;
+  startDate: Date;
+  id: string;
+}
